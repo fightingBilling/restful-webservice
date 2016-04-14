6de9ca3c80291bef358bec55706e9c23fa920bfd
love-somnus <928200207@qq.com>
2016-04-14 22:56:34 +0800

    v

--- src/main/java/com/somnus/support/ClientCase.java
+++ src/main/java/com/somnus/support/ClientCase.java
@@ -2,22 +2,16 @@ package com.somnus.support;
 
 import javax.ws.rs.core.MediaType;
 
+import org.junit.Test;
+
 import com.somnus.domain.Request;
+import com.somnus.domain.Response;
+import com.somnus.resource.RestfulResource;
+import com.somnus.rest.core.client.RESTfulJsonClientFactory;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-/**
- * @description: 
- * Copyright 2011-2016 B5M.COM. All rights reserved
- * @author:  宫柳
- * @version: 1.0
- * @createDate: 2016年4月14日 下午6:14:13 
- * Modification  History:
- * Date         Author        Version        Discription
- * -----------------------------------------------------------------------------------
- * 2016年4月14日       宫柳                               1.0            
- */
 public class ClientCase {
 
 	/**
@@ -31,6 +25,18 @@ public class ClientCase {
 		ClientResponse response = webResource.entity(new Request("admin","123456"),
 				MediaType.APPLICATION_JSON).post(ClientResponse.class);
 		System.out.println(response.getStatus());
+	}
+	
+	private static final String BASE_URL = "http://120.26.68.243:8080/restful/service/";
+	
+	RestfulResource resource = RESTfulJsonClientFactory.createClient(RestfulResource.class, BASE_URL);
+	
+	@Test
+	public void rest(){
+		
+		Response response = resource.getAccount(new Request("admin","123456"));
+		
+		System.out.println(response);
 	}
 
 }
